3054e96ad301d491406541975c6c898e763fa426
Andrew Aghoghovwia <androyt1@gmail.com>
2022-08-26 10:37:18 +0100

    added to develop

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Box from './components/Box'
 const App = () => {
   return (
     <div className='bg-slate-50 min-h-[200vh] space-y-[10vh]  pt-[50px]'>
+      <h1 className='text-3xl'>New Feature</h1>
       <Box/>      
       <Box/>
       <Box/>      
